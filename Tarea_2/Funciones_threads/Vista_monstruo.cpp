1779a335d955dbdf94a75fa5641b3d846652c2dc
0s3a5 <163148412+0s3a5@users.noreply.github.com>
2025-10-29 21:02:42 +0000

    Update Vista_monstruo.cpp

--- Tarea_2/Funciones_threads/Vista_monstruo.cpp
+++ Tarea_2/Funciones_threads/Vista_monstruo.cpp
@@ -1,15 +1,17 @@
 void comprobarVision(Monstruo &monstruo, const Heroe& heroe) {
-    lock_guard<mutex> lock(mtx); // zona crítica protegida (mapa)
-    int dx = abs(monstruo.pos.x - heroe.pos.x);
-    int dy = abs(monstruo.pos.y - heroe.pos.y);
-    bool en_rango = (dx <= monstruo.vision_range && dy <= monstruo.vision_range);
+    lock_guard<mutex> lock(mtx);
+    for(int i=(monstruo.pos.x-monstruo.vision_range); i<monstruo.pos.x+monstruo.vision_range; i++) {
+        for(int j=(monstruo.pos.y-monstruo.vision_range); i<monstruo.pos.y+monstruo.vision_range;  j++) {
+            if(mapa[i][j] == 2) {
+                int distanciaX = abs(i - heroe.pos.x);
+                int distanciaY = abs(j - heroe.pos.y);
+                int distancia = distanciaX + distanciaY;
 
-    if (en_rango && !monstruo.active) {
-        monstruo.active = true;
-        cout << "  Monstruo " << monstruo.id << " ha visto al héroe!" << endl;
+                if(distancia <= monstruo.vision_range) {
+                    monstruo.active = true;
+                    return;
+                }
+            }
+        }
     }
 }
-
---EN EL MAIN --
-
-  comprobarVision(monstruo, heroe);
