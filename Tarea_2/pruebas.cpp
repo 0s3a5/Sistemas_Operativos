e61c76b4fb0ab3e8a061378b094f2ab47209c429
0s3a5 <163148412+0s3a5@users.noreply.github.com>
2025-10-29 19:21:33 -0300

    Fix loop condition in comprobarVision function

--- Tarea_2/pruebas.cpp
+++ Tarea_2/pruebas.cpp
@@ -71,7 +71,7 @@ inline int distanciaManhattan(const Pos &a, const Pos &b) {
 void comprobarVision(Monstruo &monstruo, const Heroe &heroe) {
    lock_guard<mutex> lock(mtx);
     for(int i=(monstruo.pos.x-monstruo.vision_range); i<monstruo.pos.x+monstruo.vision_range; i++) {
-        for(int j=(monstruo.pos.y-monstruo.vision_range); i<monstruo.pos.y+monstruo.vision_range;  j++) {
+        for(int j=(monstruo.pos.y-monstruo.vision_range); j<monstruo.pos.y+monstruo.vision_range;  j++) {
             if(mapa[i][j] == 2) {
                 int distanciaX = abs(i - heroe.pos.x);
                 int distanciaY = abs(j - heroe.pos.y);
