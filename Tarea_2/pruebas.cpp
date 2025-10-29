87acdfd3479c49a78636574b6bcbff9438bfce79
0s3a5 <163148412+0s3a5@users.noreply.github.com>
2025-10-29 19:14:07 -0300

    Update pruebas.cpp

--- Tarea_2/pruebas.cpp
+++ Tarea_2/pruebas.cpp
@@ -68,10 +68,23 @@ inline int distanciaManhattan(const Pos &a, const Pos &b) {
 // =============================
 // Función de visión
 // =============================
-void comprobarVision(Monstruo &m, const Heroe &h) {
-    int dist = distanciaManhattan(m.pos, h.pos);
-    if (dist <= m.vision_range) m.active = true;
-    else m.active = false;
+void comprobarVision(Monstruo &monstruo, const Heroe &heroe) {
+   lock_guard<mutex> lock(mtx);
+    for(int i=(monstruo.pos.x-monstruo.vision_range); i<monstruo.pos.x+monstruo.vision_range; i++) {
+        for(int j=(monstruo.pos.y-monstruo.vision_range); i<monstruo.pos.y+monstruo.vision_range;  j++) {
+            if(mapa[i][j] == 2) {
+                int distanciaX = abs(i - heroe.pos.x);
+                int distanciaY = abs(j - heroe.pos.y);
+                int distancia = distanciaX + distanciaY;
+
+                if(distancia <= monstruo.vision_range) {
+                    monstruo.active = true;
+                    return;
+                }
+            }
+        }
+    }
+}
 }
 
 // =============================
