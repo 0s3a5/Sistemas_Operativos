367918592770585e65d5359bcd5ce9f99200c50e
0s3a5 <163148412+0s3a5@users.noreply.github.com>
2025-12-03 18:43:35 -0300

    Update tarea.cpp

--- Tarea_3/simple/tarea.cpp
+++ Tarea_3/simple/tarea.cpp
@@ -178,7 +178,7 @@ int main() {
     cout << "ingrese tamanñ Pagina (KB) "<<endl;
     if (!(cin >> pag_kb) || pag_kb <= 0) return 1;
     int min_proc = 1024; 
-    int max_proc = (int)((ram_mb * 1024.0) * 0.50); 
+    int max_proc = (int)((ram_mb * 1024.0) * 0.30); 
     if (max_proc < min_proc) max_proc = min_proc;
 
     cout << "rangos de procesos"<< endl;
