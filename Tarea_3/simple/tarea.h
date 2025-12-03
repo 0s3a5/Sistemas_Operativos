68a6b7d33e271167552cf3cb9b311425de273ab9
0s3a5 <163148412+0s3a5@users.noreply.github.com>
2025-12-03 18:00:29 -0300

    Rename and refactor process and memory structures

--- Tarea_3/simple/tarea.h
+++ Tarea_3/simple/tarea.h
@@ -2,63 +2,56 @@
 #include <vector>
 #include <deque>
 #include <string>
-
 using namespace std;
 
-// --- Estructuras de Proceso ---
-struct PageInfo {
-    int page_number;
-    bool in_ram;
-    int frame_index;
-    bool in_swap;
-    int swap_index;
+struct InfoPag {
+    int id_pag;     
+    bool en_ram;   
+    int marco_ram; 
+    bool en_swap;   
+    int idx_swap;    
 };
 
-class Process {
+class Proceso {
 public:
     int pid;
-    int size_kb;
-    int num_pages;
-    vector<PageInfo> pages;
+    int tam_kb;
+    int num_pags;
+    vector<InfoPag> pags;
 
-    Process(int pid, int size_kb, int page_size_kb);
+    Proceso(int id, int tam, int tam_pag);
 };
 
 // --- Estructuras de Memoria ---
-struct Frame {
-    bool used;
+
+struct Marco {
+    bool uso;
     int pid;
-    int page_number;
-    bool is_swap;
+    int id_pag;
 };
 
-class MemoryManager {
+class GestorMem {
 public:
-    int page_size_kb;
-    int frames_ram;
-    int frames_swap;
+    int tam_pag;
+    int total_ram;    
+    int total_swap;   
 
-    vector<Frame> ram;
-    vector<Frame> swap;
-    deque<int> fifo_order; // Para política FIFO
+    vector<Marco> ram;
+    vector<Marco> swap;
+    deque<int> cola_fifo; 
 
-    MemoryManager(int page_size_kb, int frames_ram, int frames_swap);
+    GestorMem(int tam_p, int t_ram, int t_swap);
 
-    bool allocate_process(Process &proc);
-    void free_process(const Process &proc);
-
-    bool access_page(vector<Process> &processes,
-                     Process &proc,
-                     int page_index,
-                     bool verbose = true);
-
-    int free_ram_frames() const;
-    int free_swap_frames() const;
+    bool asignar(Proceso &p);
+    void liberar(const Proceso &p);
+    bool acceder(vector<Proceso> &lista_procs, Proceso &p, int n_pag);
     
-    // --- NUEVO: Interfaz de estado del Trabajo A ---
-    void print_status() const; 
+  
+    void estado() const;
+    int libres_ram() const;
+    int libres_swap() const;
 
 private:
-    int find_free_ram_frame() const;
-    int find_free_swap_frame() const;
+    int buscar_ram() const;
+    int buscar_swap() const;
 };
